61a482a7271a5c31c4475d518b9c2cd1c2854dac
Chaunk <jcu531@naver.com>
2020-12-31 11:38:16 +0900

    클래스화

--- TemHum/TemHum/Program.cs
+++ TemHum/TemHum/Program.cs
@@ -11,38 +11,58 @@ namespace TemHum
     {
         static void Main(string[] args)
         {
-            SerialPort SP = new SerialPort();
-            var isOpen = SerialSetting(SP);
+            var temHum = new TemHum();
+            temHum.OpenSerial();
 
-            SP.Close();
-            SP.Open();
+            Console.WriteLine("아무 키나 누르세요...");
+            Console.ReadKey();           
+        }
+    }
 
-            isOpen = SP.IsOpen;
+    class TemHum
+    {
+        private static SerialPort SerialPort;
+
+        public void OpenSerial()
+        {
+            SerialPort = new SerialPort();
+            var isOpen = SetSerialPort(SerialPort);
             if (isOpen)
-            {                   
+            {
+                SerialPort.Close();
+                SerialPort.Open();
+            }
+            else
+            {
+                Console.WriteLine("포트가 없습니다.");
+            }
+
+            isOpen = SerialPort.IsOpen;
+            if (isOpen)
+            {
                 while (true)
                 {
                     string sendMsg = ":80040004000375\r\n";
-                    SP.Write(sendMsg);
+                    SerialPort.Write(sendMsg);
                     Thread.Sleep(500);
-                    string data = SP.ReadExisting();
+                    string data = SerialPort.ReadExisting();
                     if (data != string.Empty)
-                    {                      
+                    {
                         PrintTemHum(data);
                     }
-                }            
+                }
             }
-
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            SP.Close();
+            else
+            {
+                Console.WriteLine("포트가 열리지 않았습니다.");
+            }
+            SerialPort.Close();
         }
 
-        public static bool SerialSetting(SerialPort serialPort)
+        private static bool SetSerialPort(SerialPort serialPort)
         {
-            var portNum = 0;
-            Console.Write("포트 번호를 입력하세요: COM");          
-            int.TryParse(Console.ReadLine(), out portNum);
+            Console.Write("포트 번호를 입력하세요: COM");
+            int.TryParse(Console.ReadLine(), out int portNum);
 
             serialPort.PortName = "COM" + portNum;
             serialPort.BaudRate = 9600;
@@ -52,12 +72,12 @@ namespace TemHum
             serialPort.Handshake = Handshake.None;
             serialPort.RtsEnable = true;
 
-            var portCheck = SerialPort.GetPortNames();           
+            var portCheck = SerialPort.GetPortNames();
             var checkPort = portCheck.Contains(serialPort.PortName);
             return checkPort;
         }
 
-        public static void PrintTemHum(string item)
+        private static void PrintTemHum(string item)
         {
             string temp = item.Substring(11, 4);
             string humi = item.Substring(7, 4);
